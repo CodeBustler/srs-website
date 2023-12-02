f008b31e7cf6ffa03acb0e3c6e7138a265e59bd8
CodeBustler <codebustler360online@gmail.com>
2023-12-03 02:01:52 +0530

    FILES ADDED

---
+++ script.js
@@ -0,0 +1,32 @@
+// Define all UI variable
+const navToggler = document.querySelector(".nav-toggler");
+const navMenu = document.querySelector(".site-navbar ul");
+const navLinks = document.querySelectorAll(".site-navbar a");
+const copyrightYear = document.getElementById("copyright-year");
+
+// Dynamic Year
+copyrightYear.innerText = new Date().getFullYear();
+
+// load all event listners
+allEventListners();
+
+// functions of all event listners
+function allEventListners() {
+  // toggler icon click event
+  navToggler.addEventListener("click", togglerClick);
+  // nav links click event
+  navLinks.forEach((elem) => elem.addEventListener("click", navLinkClick));
+}
+
+// togglerClick function
+function togglerClick() {
+  navToggler.classList.toggle("toggler-open");
+  navMenu.classList.toggle("open");
+}
+
+// navLinkClick function
+function navLinkClick() {
+  if (navMenu.classList.contains("open")) {
+    navToggler.click();
+  }
+}
